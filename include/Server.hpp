4be2a6c58e8c926bf3ebd757ba83f44ff8f122ac
Stephie <sdemaude@student.42lehavre.fr>
2024-10-22 15:29:01 +0200

    add Server class

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,25 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdemaude <sdemaude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 13:55:18 by sdemaude          #+#    #+#             */
-/*   Updated: 2024/10/22 14:27:50 by sdemaude         ###   ########.fr       */
+/*   Created: 2024/10/22 14:41:37 by sdemaude          #+#    #+#             */
+/*   Updated: 2024/10/22 14:41:55 by sdemaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/include.hpp"
+#pragma once
 
-int main(int argc, char **argv)
-{
-    if (argc != 3)
-    {
-        std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
-        return 1;
-    }
-    // argv[1] = port
-    // argv[2] = password
-    return 0;
-}
+#include "include.hpp"
--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sdemaude <sdemaude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 13:55:18 by sdemaude          #+#    #+#             */
-/*   Updated: 2024/10/22 14:27:50 by sdemaude         ###   ########.fr       */
+/*   Created: 2024/10/22 14:41:14 by sdemaude          #+#    #+#             */
+/*   Updated: 2024/10/22 14:41:59 by sdemaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/include.hpp"
+#include "../include/Server.hpp"
 
-int main(int argc, char **argv)
+class Server
 {
-    if (argc != 3)
-    {
-        std::cerr << "Usage: ./ircserv <port> <password>" << std::endl;
-        return 1;
-    }
-    // argv[1] = port
-    // argv[2] = password
-    return 0;
-}
+    public:
+        Server();
+        ~Server();
+    private:
+        int _port;
+        std::string _password;
+};
