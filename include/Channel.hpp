de8a2b01e4f681d6fc5f9e2608a62bb0b9434b3d
Stephie <sdemaude@student.42lehavre.fr>
2024-10-25 18:05:53 +0200

    add channel class

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -6,10 +6,25 @@
 /*   By: sdemaude <sdemaude@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:47:27 by sdemaude          #+#    #+#             */
-/*   Updated: 2024/10/25 15:56:37 by sdemaude         ###   ########.fr       */
+/*   Updated: 2024/10/25 18:04:15 by sdemaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "ft_irc.hpp"
+
+class Channel
+{
+	public:
+		Channel();
+		~Channel();
+
+    private:
+		std::string					_name;
+		std::string					_topic;
+		std::string					_password;
+		std::vector<std::string>	_users;
+		// std::string				_mode;
+		// std::string				_limit;
+};
